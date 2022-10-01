c54fba057911c5fd2470924fb6a039a69eabed36
Ben Finch <60013890+EngineerBug@users.noreply.github.com>
2022-10-01 11:14:09 +0100

    Replace database login details

--- db_login.php
+++ db_login.php
@@ -1,9 +1,9 @@
 <?php
-$username = "root";
-$password = "HaloBenX";
-$dbname = "tetris";
+$username = "year_username";
+$password = "your_password";
+$dbname = "your_database_name";
 // Create connection
-$conn = mysqli_connect("localhost", $username, $password, $dbname);
+$conn = mysqli_connect("database_location", $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . mysqli_connect_error());
